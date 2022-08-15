135f37bea241560a3f32f52e052f1b3f130ba85f
Carin Wood <carin.wood.85@gmail.com>
2022-08-15 10:37:53 +0200

    footer update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
-
+import styles from '../styles/footer.module.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={styles.footer}>
+        <p>Copyright @2022 Getit Nordic</p>
+    </footer>
   )
 }
 
