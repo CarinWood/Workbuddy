d1656c053fb2a171ee468f76d00ab7fb70fd3fa8
Carin Wood <carin.wood.85@gmail.com>
2022-08-15 10:25:33 +0200

    navbar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,20 @@
-
+import styles from '../styles/navbar.module.scss'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={styles.navbar}>
+        <div>
+            Logo
+        </div>
+
+        <div className={styles.links}>
+            <Link href="/"><a className={styles.navlink}>Home</a></Link>
+            <Link href="/about"><a className={styles.navlink}>About</a></Link>
+            <Link href="/services"><a className={styles.navlink}>Services</a></Link>
+            <Link href="/contact"><a className={styles.navlink}>Contact</a></Link>
+        </div>
+    </nav>
   )
 }
 
